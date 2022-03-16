06571e0b685f9094afc75ce8d3d15653a72f919f
Sal Tornetto <saltorntto1@gmail.com>
2022-03-16 11:27:16 -0400

    api progress

---
+++ server/api.js
@@ -0,0 +1,67 @@
+//Amazon api code -- Hard Limit 200/month
+
+//this is only for minecraft as shown in the url having product id B07D13QGXM
+const request = require('request');
+
+const options = {
+  method: 'GET',
+  url: 'https://amazon24.p.rapidapi.com/api/product/B07D13QGXM',
+  qs: {country: 'US'},
+  headers: {
+    'x-rapidapi-host': 'amazon24.p.rapidapi.com',
+    'x-rapidapi-key': '098fb763bemshd82bc6a03bb30b5p1c44dbjsn5df752daea73',
+    useQueryString: true
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
+
+
+//Target api code -- Hard Limit 50/month
+
+//this is for legends arceus shown by having the tcin to 83617997
+const request = require('request');
+
+const options = {
+  method: 'GET',
+  url: 'https://target-com-store-product-reviews-locations-data.p.rapidapi.com/product/details',
+  qs: {store_id: '3991', tcin: '83617997'},
+  headers: {
+    'x-rapidapi-host': 'target-com-store-product-reviews-locations-data.p.rapidapi.com',
+    'x-rapidapi-key': '098fb763bemshd82bc6a03bb30b5p1c44dbjsn5df752daea73',
+    useQueryString: true
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
+
+
+//walmart api for pokemon egends arcues  -    Hard limit 500/month   (make sure your are using get-details-v3!)
+const request = require('request');
+
+const options = {
+  method: 'GET',
+  url: 'https://walmart.p.rapidapi.com/products/v3/get-details',
+  //usItemId is present at the end of the url for an item and serves as the look up 
+  qs: {usItemId: '890161998'},
+  headers: {
+    'x-rapidapi-host': 'walmart.p.rapidapi.com',
+    'x-rapidapi-key': '098fb763bemshd82bc6a03bb30b5p1c44dbjsn5df752daea73',
+    useQueryString: true
+  }
+};
+
+request(options, function (error, response, body) {
+	if (error) throw new Error(error);
+
+	console.log(body);
+});
+
